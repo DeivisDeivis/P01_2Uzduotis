a3b185acf09d0fefc3ce1c1eab4dde57fe17e384
DeivisDeivis <145267334+DeivisDeivis@users.noreply.github.com>
2023-09-17 15:04:11 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P01_2Uzduotis
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double Fxy;
+
+            Console.WriteLine("Iveskite x reiksme");
+           double x= double.Parse(Console.ReadLine());
+            
+            Console.WriteLine("Iveskite y reiksme ");
+                double y = double.Parse(Console.ReadLine());
+
+            Fxy = ((y * y)- (2*y*x) + (x*x))/ ((x*x*x)-y);
+            Console.WriteLine(Fxy);
+
+        }
+    }
+}
